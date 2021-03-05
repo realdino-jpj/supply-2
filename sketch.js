ef3cf906ed5195f86e1690deff6d6f2883f5d623
realdino-jpj <78307958+realdino-jpj@users.noreply.github.com>
2021-03-05 05:10:14 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,78 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var stick1;
+var stick2;
+var stick3;
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+
+
+	packageBody = Bodies.circle(width/2 , 200 , 5,{restitution:0.6} );
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+ 
+
+	Engine.run(engine);
+	//stick1=new Cage(200,200,70,10);
+	stick1 = new Cage(350,650,100,10);
+	stick2 = new Cage(400,600,10,100);
+	stick3 = new Cage(300,600,10,100);
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  Engine.update(engine);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  keyPressed();
+  
+ stick1.display();
+ stick2.display();
+ stick3.display();
+ drawSprites();
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
+	
+    
+  }
+}
+
+
+
